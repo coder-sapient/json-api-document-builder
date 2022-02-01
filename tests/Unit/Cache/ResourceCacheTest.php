881cad3ce810deda862acf7b651e36b8de7600ca
«yarikhalupiak» <i.am.khalupiak@gmail.com>
2022-02-01 02:20:36 +0200

    added flush method

--- tests/Unit/Cache/ResourceCacheTest.php
+++ tests/Unit/Cache/ResourceCacheTest.php
@@ -14,26 +14,54 @@ final class ResourceCacheTest extends TestCase
     /** @test */
     public function in_memory_resource_cache(): void
     {
-        $resource = ResourceMother::create('1', 'articles');
+        $resource1 = ResourceMother::create('1', 'articles');
+        $resource2 = ResourceMother::create('2', 'articles');
+        $resource3 = ResourceMother::create('3', 'articles');
+
         $criteria = CriteriaMother::create();
+
         $cache = new InMemoryResourceCache();
 
-        self::assertNull($cache->getOne($resource->key()));
-        self::assertEmpty($cache->getMany($resource->key()));
+        self::assertNull($cache->getOne($resource1->key()));
+        self::assertEmpty($cache->getMany($resource1->key()));
         self::assertEmpty($cache->getByCriteria('articles', $criteria));
 
-        $cache->set($resource);
-        $cache->setByCriteria('articles', $criteria, $resource);
+        $cache->set($resource1);
+        $cache->set($resource2);
+        $cache->setByCriteria('articles', $criteria, $resource3);
 
-        self::assertSame($resource, $cache->getOne($resource->key()));
-        self::assertSame([$resource], $cache->getMany($resource->key()));
-        self::assertSame([$resource], $cache->getByCriteria('articles', $criteria));
+        self::assertSame($resource1, $cache->getOne($resource1->key()));
+        self::assertSame($resource2, $cache->getOne($resource2->key()));
+        self::assertSame([$resource1, $resource2], $cache->getMany($resource1->key(), $resource2->key()));
+        self::assertSame([$resource3], $cache->getByCriteria('articles', $criteria));
 
-        $cache->remove($resource->key());
+        $cache->remove($resource1->key());
+        $cache->remove($resource2->key());
         $cache->removeByCriteria('articles', $criteria);
 
-        self::assertNull($cache->getOne($resource->key()));
-        self::assertEmpty($cache->getMany($resource->key()));
+        self::assertNull($cache->getOne($resource1->key()));
+        self::assertNull($cache->getOne($resource2->key()));
+        self::assertEmpty($cache->getMany($resource1->key(), $resource2->key()));
         self::assertEmpty($cache->getByCriteria('articles', $criteria));
+
+        $cache->set($resource1, $resource2, $resource3);
+
+        self::assertSame(
+            [
+                $resource1,
+                $resource2,
+                $resource3,
+            ],
+            $cache->getMany(
+                $resource1->key(),
+                $resource2->key(),
+                $resource3->key(),
+            ),
+        );
+
+
+        $cache->flush();
+
+        self::assertEmpty($cache->getMany($resource1->key(), $resource2->key(), $resource3->key()));
     }
 }
