910881df31adfd3349524bfab516a0a5b4ec2800
yarikhalupiak <i.am.khalupiak@gmail.com>
2022-02-02 18:08:00 +0200

    updated cache api

--- src/Cache/InMemoryResourceCache.php
+++ src/Cache/InMemoryResourceCache.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace CoderSapient\JsonApi\Cache;
 
 use CoderSapient\JsonApi\Criteria\Criteria;
+use CoderSapient\JsonApi\Utils;
 use JsonApiPhp\JsonApi\ResourceObject;
 
 class InMemoryResourceCache implements ResourceCache
@@ -13,7 +14,7 @@ class InMemoryResourceCache implements ResourceCache
 
     public function getByKey(string $key): ?ResourceObject
     {
-        return $this->cache['keys'][$key] ?? null;
+        return $this->cache[Utils::typeFromKey($key)]['keys'][$key] ?? null;
     }
 
     /**
@@ -24,8 +25,9 @@ class InMemoryResourceCache implements ResourceCache
         $resources = [];
 
         foreach ($keys as $key) {
-            if (isset($this->cache['keys'][$key])) {
-                $resources[] = $this->cache['keys'][$key];
+            $resourceType = Utils::typeFromKey($key);
+            if (isset($this->cache[$resourceType]['keys'][$key])) {
+                $resources[] = $this->cache[$resourceType]['keys'][$key];
             }
         }
 
@@ -37,37 +39,34 @@ class InMemoryResourceCache implements ResourceCache
      */
     public function getByCriteria(string $resourceType, Criteria $criteria): array
     {
-        return $this->cache['criteria'][$resourceType][$criteria->key()] ?? [];
+        return $this->cache[$resourceType]['criteria'][$criteria->key()] ?? [];
     }
 
     public function setByKeys(ResourceObject ...$resources): void
     {
         foreach ($resources as $resource) {
-            $this->cache['keys'][$resource->key()] = $resource;
+            $key = $resource->key();
+            $this->cache[Utils::typeFromKey($key)]['keys'][$key] = $resource;
         }
     }
 
     public function setByCriteria(string $resourceType, Criteria $criteria, ResourceObject ...$resources): void
     {
-        $this->cache['criteria'][$resourceType][$criteria->key()] = $resources;
+        $this->cache[$resourceType]['criteria'][$criteria->key()] = $resources;
     }
 
     public function removeByKeys(string ...$keys): void
     {
         foreach ($keys as $key) {
-            unset($this->cache['keys'][$key]);
+            unset($this->cache[Utils::typeFromKey($key)]['keys'][$key]);
         }
     }
 
-    public function removeByType(string $resourceType): void
+    public function removeByTypes(string ...$resourceTypes): void
     {
-        foreach ($this->cache['keys'] as $key => $resource) {
-            if (str_starts_with($key, $resourceType)) {
-                unset($this->cache['keys'][$key]);
-            }
+        foreach ($resourceTypes as $resourceType) {
+            unset($this->cache[$resourceType]);
         }
-
-        unset($this->cache['criteria'][$resourceType]);
     }
 
     public function flush(): void
